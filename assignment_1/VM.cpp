6fec3dede49891ca1d2cdaa866ef80d300293ae5
LamTS72 <sonlam7220@gmail.com>
2022-05-23 12:42:12 +0700

    initial commit

--- assignment_1/VM.cpp
+++ assignment_1/VM.cpp
@@ -1,6 +1,234 @@
 #include "VM.h"
 
+//TrinhSonLam
+void VM:: loadFile(string filename){
+ifstream myfile(filename);
+        string s;
+        while (getline(myfile,s)){
+            vector <string> line;
+            string part;
+            int countspace=0;
+            for (int indx=0; indx<(int)s.size(); indx++){
+                 if (s[indx]==' '){                             //nếu chuỗi s tại vị trí idx có space
+                    if(indx==0 || indx==(int)(s.size()-1))  throw InvalidInstruction(countINS);                      
+                        ++countspace;
+                        line.push_back(part);                      // line là dòng thứ nào đó  sẽ đầy chuỗi item 
+                        part.clear();                               // chuổi part sau đó sẽ dc xoá đi 
+                }                                          
+                else part.push_back(s[indx]);
+            }
+            line.push_back(part);  // line =  [INS] ->[DST,] ->[SRC]
+            if(line[0]=="Return"|| line[0]=="Halt"){
+                if(countspace>0) throw InvalidInstruction(countINS);
+            }
+            else if(line[0]=="Not"||line[0]=="Output"|| line[0]=="Input"||line[0]=="Jump"||line[0]=="Call"){
+                if(countspace>1||countspace <=0) throw InvalidInstruction(countINS);
+            }
+            else{
+                if(countspace>2 || countspace <=1) throw InvalidInstruction(countINS); 
+            }
+            readINS.push_back(line);
+            countINS++;
+    } 
+};
+VM::dataType VM::srcData(string src){
+        dataType reg;
+        if(src[0] == 'R'){
+          int i=stoi(src.substr(1));
+            return R[i];
+    	}
+        
+        if (src.find('.')!= string::npos){
+            reg.type=Float;                     
+            reg.data.dFloat=stof(src);
+        }
+        else if (src.find('A')!= string::npos){
+            reg.type=Addr;
+            src.pop_back();             //bỏ đi ký tự A ở cuối chuỗi
+            reg.data.dAddr=stoi(src);
+        }
+        else if (isdigit(src[0])){              
+            reg.type=Int;
+            reg.data.dInt=stoi(src);
+        }
+        else {
+            reg.type=Bool;
+            if (src=="TRUE")
+            reg.data.dBool=true;
+            else reg.data.dBool=false;
+        }
+        return reg; 
+};
+
+void VM:: execuProgram(){
+
+    smatch matchs;
+    regex ARITHadd("Add");regex ARITHminus("Minus");regex ARITHmul("Mul");regex ARITHdiv("Div");
+    regex CMP("CmpEQ|CmpNE|CmpLT|CmpLE|CmpGT|CmpGE");
+    regex CMPeq("CmpEQ");regex CMPne("CmpNE");regex CMPlt("CmpLT");regex CMPle("CmpLE");regex CMPgt("CmpGT");regex CMPge("CmpGE");
+    regex LOGnot("Not"); regex LOGand("And");regex LOGor("Or");
+    regex REWRmove("Move");regex REWRload("Load");regex REWRstore("Store");
+    regex CTRjump("Jump");regex CTRjumpif("JumpIf");regex CTRcall("Call");regex CTRreturn("Return");regex CTRhalt("Halt");
+    regex IOPip("Input");regex IOPop("Output");
+
+
+	while (true){
+            vector<string> part = readINS[IP];
+            int ip=IP;
+            IP++;
+
+               if (regex_match(part[0],matchs,ARITHadd)) {
+                    R[stoi(part[1].substr(1))]+=srcData(part[2]);
+                }
+                else if (regex_match(part[0],matchs,ARITHminus)) {
+                    R[stoi(part[1].substr(1))]-=(srcData(part[2]));
+                }
+                else if (regex_match(part[0],matchs,ARITHmul)) {
+                    R[stoi(part[1].substr(1))]*=(srcData(part[2]));
+                }
+                else if (regex_match(part[0],matchs,ARITHdiv)) {
+                    if(srcData(part[2]).data.dInt == 0 || srcData(part[2]).data.dFloat == 0.0 ) throw DivideByZero(ip);
+                    R[stoi(part[1].substr(1))]/=(srcData(part[2]));
+                }
+            // Compare
+                else if (regex_match(part[0],matchs,CMPeq)) {
+                    int i=stoi(part[1].substr(1));
+                    R[i].data.dBool=(R[i]==(srcData(part[2])));
+                    R[i].type = Bool;
+                }
+                else if (regex_match(part[0],matchs,CMPne)) {
+                    int i=stoi(part[1].substr(1));
+                    R[i].data.dBool=(R[i]!=(srcData(part[2])));
+                    R[i].type = Bool;
+                }
+                else if (regex_match(part[0],matchs,CMPlt)) {
+                    int i=stoi(part[1].substr(1));
+                    R[i].data.dBool=(R[i]<(srcData(part[2])));
+                    R[i].type = Bool;
+                }
+                else if (regex_match(part[0],matchs,CMPle)) {
+                    int i=stoi(part[1].substr(1));
+                    R[i].data.dBool=(R[i]<=(srcData(part[2])));
+                    R[i].type = Bool;
+                }
+                else if (regex_match(part[0],matchs,CMPgt)) {
+                    int i=stoi(part[1].substr(1));
+                    R[i].data.dBool=(R[i]>(srcData(part[2])));
+                    R[i].type = Bool;
+                }
+                else if (regex_match(part[0],matchs,CMPge)) {
+                    int i=stoi(part[1].substr(1));
+                    R[i].data.dBool=(R[i]>=(srcData(part[2])));
+                    R[i].type = Bool;
+                }
+
+            // Logic
+                else if (regex_match(part[0],matchs,LOGnot)) {
+                    int i=stoi(part[1].substr(1));
+                    if(R[i].type != Bool){
+                        throw TypeMismatch(ip);
+                    }
+                    else
+                        R[i].data.dBool = !(R[i].data.dBool);
+                }
+
+                else if (regex_match(part[0],matchs,LOGand)) {
+                    int i=stoi(part[1].substr(1));
+                    if(R[i].type != Bool){
+                        throw TypeMismatch(ip);
+                    }
+                    else
+                        R[i].data.dBool = (R[i].data.dBool&&srcData(part[2]).data.dBool);
+                }
+
+                else if (regex_match(part[0],matchs,LOGor)) {
+                    int i=stoi(part[1].substr(1));
+                    if(R[i].type != Bool){
+                        throw TypeMismatch(ip);
+                    }
+                    else
+                        R[i].data.dBool = (R[i].data.dBool||srcData(part[2]).data.dBool);
+                }
+
+                
+            // Load and Store
+                else if (regex_match(part[0],matchs,REWRmove)) {
+                    int i=stoi(part[1].substr(1));
+                    R[i] = srcData(part[2]);
+                }
+                else if (regex_match(part[0],matchs,REWRload)) {
+                    if(srcData(part[2]).type != Addr){
+
+                        throw TypeMismatch(ip);
+                    }
+                    else{
+                        int i=stoi(part[1].substr(1));
+                        R[i] = static_Mem[srcData(part[2]).data.dAddr];
+                    }
+                }
+                else if (regex_match(part[0],matchs,REWRstore)) {
+                    int i=stoi(part[1].substr(1));
+                    if(R[i].type != Addr){
+                        throw TypeMismatch(ip);
+                    }
+                    else{
+                        static_Mem[R[i].data.dAddr] = srcData(part[2]);
+                    }
+                }
+
+
+            //control
+                else if (regex_match(part[0],matchs,CTRjump)) {
+                    if(srcData(part[1]).data.dAddr > countINS||srcData(part[1]).data.dAddr < 0 ) throw InvalidDestination(ip);
+                    IP = srcData(part[1]).data.dAddr;
+                }
+                else if (regex_match(part[0],matchs,CTRjumpif)) {
+                    int i=stoi(part[1].substr(1));
+                    if(R[i].data.dBool){
+                      if (srcData(part[2]).data.dAddr > countINS || srcData(part[2]).data.dAddr <0) throw InvalidDestination(ip);
+                     IP = srcData(part[2]).data.dAddr;
+                   }  
+                }
+                else if (regex_match(part[0],matchs,CTRcall)) {
+                    Stack.push_back(IP);
+                    if(Stack.size()== 999) throw StackFull(ip);
+                    if(srcData(part[1]).data.dAddr > countINS || srcData(part[1]).data.dAddr <0) throw InvalidDestination(ip);
+                    IP = srcData(part[1]).data.dAddr;
+                }
+                else if (regex_match(part[0],matchs,CTRreturn)) {
+                    IP = Stack.back();
+                    Stack.pop_back();
+                }
+                else if (regex_match(part[0],matchs,CTRhalt)) {
+                    return;
+                }
+
+                //...06
+            // Inp and Outp
+                else if (regex_match(part[0],matchs,IOPip)) {
+                    string s;cin>>s;
+                    if(s.find('A')!= string::npos) throw InvalidInput(ip);
+                    int i=stoi(part[1].substr(1));
+                    R[i]=srcData(s);
+                }
+                else if (regex_match(part[0],matchs,IOPop)) {
+                    dataType out = srcData(part[1]);
+                
+                    if(out.type == Int) cout<<out.data.dInt;
+                    if(out.type == Float) cout<<out.data.dFloat;
+                    if(out.type == Addr) cout<<out.data.dAddr<<"A";
+                    if(out.type == Bool) cout<<((out.data.dBool)?"true":"false");
+                }
+                //...
+                else  throw InvalidInstruction(ip);
+                
+        }
+}
+
 void VM::run(string filename)
 {
-	cout << 1;
+ 
+	loadFile(filename);
+    dataType srcData();
+    execuProgram();
 }
--- assignment_1/VM.h
+++ assignment_1/VM.h
@@ -2,68 +2,240 @@
 #define VM_H
 
 #include "main.h"
-#define MAX_STACK 1000;
-typedef uint16_t;
 
+#define MAX_SIZE 1000;
+        
+static const int vnMemory=65536;
+  
+enum Type {None,Int,Float, Addr, Bool};
+union Data{
 
-enum Instruction{
-  ADD,MINUS,MUL,DIV,CmpEQ,CmpNE,CmpLT,CmpLE,CmpGT,CmpGE,
-  Not,And,Or,Move,Load,Store,Jump,JumpIf,Call,Return,Halt,
-  Input,Output
+        int dInt;
+        float dFloat;
+        int dAddr;
+        bool dBool;
 };
-
-union Typedata{//0 no src
-  int dataInt; //1
-  float dataFlo;//2
-  bool dataBoo;//3
-  int address;//4
-};
-
-struct Data{
-  Typedata data;
-  int dataType=0;
-};
-////////////
-
-class CPU{
-public:
-Instruction opcode;
-int dest;// 1-15 generalregister , 0 InvalidOperand, 16 no dest
-Data src;
-void checkInstruction(string ins,regex reg, int error){git 
-  regex add("^Add "); regex minus("^Minus "); regex mul("^Mul "); regex div("^Div ");
-  regex cmpEQ("^CmpEQ "); regex cmpNE("^CmpNE "); regex cmpLT("^CmpLT "); regex cmpLE("^CmpLE ");
-  regex cmpGT("^CmpGT"); regex cmpGE("^CmpGE "); regex not("^Not "); regex and("^And ");
-  regex or("^Or "); regex move("^Move "); regex load("^Load "); regex store("^Store ");
-  regex jump("^Jump "); regex jumpIf("^JumIf "); regex call("^Call "); regex back("^Return ");
-  regex halt("^Halt "); regex inp("^Input "); regex out("^Output ");
-  if(regex_match(ins.begin(),ins.end(),add)) opcode=ADD;
-  else if(regex_match(ins.begin(),ins.end(),minus)) opcode=MINUS;
-  else if(regex_match(ins.begin(),ins.end(),mul)) opcode=MUL;
-  else if(regex_match(ins.begin(),ins.end(),div)) opcode=DIV;
-  else if(regex_match(ins.begin(),ins.end(),cmpEQ)) opcode=CmpEQ;
-  else if(regex_match(ins.begin(),ins.end(),cmpNE)) opcode=CmpNE;
-  else if(regex_match(ins.begin(),ins.end(),cmpLT)) opcode=CmpLT;
-  else if(regex_match(ins.begin(),ins.end(),cmpLE)) opcode=CmpLE;
-  else if(regex_match(ins.begin(),ins.end(),cmpGT)) opcode=CmpGT;
-  else if(regex_match(ins.begin(),ins.end(),cmpGE)) opcode=CmpGE;
-  else if(regex_match(ins.begin(),ins.end(),not))   opcode=Not;
-  else if(regex_match(ins.begin(),ins.end(),and))   opcode=And;
-  else if(regex_match(ins.begin(),ins.end(),or))    opcode=Or;
-  else if(regex_match(ins.begin(),ins.end(),move))  opcode=Move;
-  else if(regex_match(ins.begin(),ins.end(),load))  opcode=Load;
-  else if(regex_match(ins.begin(),ins.end(),store)) opcode=Store;
-}
-}
-
 
 
 class VM
-{
-public:
-  VM(){}
-  void run(string filename);
+{  
+    public:
+      struct dataType{
+        Type type = None;
+        Data data;
+        
+        void operator=(const dataType & realdata) {
+            this->type = realdata.type;
+            this->data = realdata.data;
+        }
+
+        void operator+=(const dataType & realdata){
+            if (this->type==Float || realdata.type==Float) {
+                float res=0;
+                if (this->type==Float) res+=this->data.dFloat;
+                else res+=this->data.dInt;
+                if (realdata.type==Float) res+=realdata.data.dFloat;
+                else res+=realdata.data.dInt;
+
+                this->type=Float;
+                this->data.dFloat=res;
+            }
+            else {
+                this->type=Int;
+                this->data.dInt+=realdata.data.dInt;
+            }
+        }
+    
+       
+        void operator-=(const dataType & realdata){
+            if (this->type==Float || realdata.type==Float) {
+                float res=0;
+                if (this->type==Float) res-=this->data.dFloat;
+                else res-=this->data.dInt;
+                if (realdata.type==Float) res-=realdata.data.dFloat;
+                else res-=realdata.data.dInt;
+
+                this->type=Float;
+                this->data.dFloat=res;
+            }
+            else {
+                this->type=Int;
+                this->data.dInt-=realdata.data.dInt;
+            }
+        }
+         void operator*=(const dataType & realdata){
+            if (this->type==Float || realdata.type==Float) {
+                float res=0;
+                if (this->type==Float) res*=this->data.dFloat;
+                else res*=this->data.dInt;
+                if (realdata.type==Float) res*=realdata.data.dFloat;
+                else res*=realdata.data.dInt;
+
+                this->type=Float;
+                this->data.dFloat=res;
+            }
+            else {
+                this->type=Int;
+                this->data.dInt*=realdata.data.dInt;
+            }
+        }
+        void operator/=(const dataType & realdata){
+            if (this->type==Float || realdata.type==Float) {
+                float res=0;
+                if (this->type==Float) res/=this->data.dFloat;
+                else res/=this->data.dInt;
+                if (realdata.type==Float) res/=realdata.data.dFloat;
+                else res/=realdata.data.dInt;
+
+                this->type=Float;
+                this->data.dFloat=res;
+            }
+            else {
+                this->type=Int;
+                this->data.dInt/=realdata.data.dInt;
+            }
+        }
+        bool operator==(const dataType & realdata){
+            bool res=false;
+            if(this->type == realdata.type){
+                if(this->type == Int) {
+                    if(this->data.dInt == realdata.data.dInt) res=true;
+                    else res=false;
+                }
+                else if(this->type == Float) {
+                    if(this->data.dFloat == realdata.data.dFloat) res=true;
+                    else res=false;
+                }
+                else if(this->type == Bool) {
+                    if(this->data.dBool == realdata.data.dBool) res=true;
+                    else res=false;
+                }
+                return res;
+            }
+            else 
+                return false;
+           
+        }
+        bool operator!=(const dataType & realdata){
+            bool res=false;
+            if(this->type == realdata.type){
+                if(this->type == Int) {
+                    if(this->data.dInt != realdata.data.dInt) res=true;
+                    else res=false;
+                }
+                else if(this->type == Float) {
+                    if(this->data.dFloat != realdata.data.dFloat) res=true;
+                    else res=false;
+                }
+                else if(this->type == Bool) {
+                    if(this->data.dBool != realdata.data.dBool) res=true;
+                    else res=false;
+                }
+                return res;
+            }
+            else 
+                return false;
+          
+        }
+        bool operator<(const dataType & realdata){
+            bool res=false;
+            if(this->type == realdata.type){
+                if(this->type == Int) {
+                    if(this->data.dInt < realdata.data.dInt) res=true;
+                    else res=false;
+                }
+                else if(this->type == Float) {
+                    if(this->data.dFloat < realdata.data.dFloat) res=true;
+                    else res=false;
+                }
+                else if(this->type == Bool) {
+                    if(this->data.dBool < realdata.data.dBool) res=true;
+                    else res=false;
+                }
+                return res;
+            }
+            else 
+                return false;
+          
+        }
+        bool operator<=(const dataType & realdata){
+            bool res=false;
+            if(this->type == realdata.type){
+                if(this->type == Int) {
+                    if(this->data.dInt <= realdata.data.dInt) res=true;
+                    else res=false;
+                }
+                else if(this->type == Float) {
+                    if(this->data.dFloat <= realdata.data.dFloat) res=true;
+                    else res=false;
+                }
+                else if(this->type == Bool) {
+                    if(this->data.dBool <= realdata.data.dBool) res=true;
+                    else res=false;
+                }
+                return res;
+            }
+            else 
+                return false;
+            
+        }
+        bool operator>(const dataType & realdata){
+            bool res=false;
+            if(this->type == realdata.type){
+                if(this->type == Int) {
+                    if(this->data.dInt > realdata.data.dInt) res=true;
+                    else res=false;
+                }
+                else if(this->type == Float) {
+                    if(this->data.dFloat > realdata.data.dFloat) res=true;
+                    else res=false;
+                }
+                else if(this->type == Bool) {
+                    if(this->data.dBool > realdata.data.dBool) res=true;
+                    else res=false;
+                }
+               return res;
+            }
+            else 
+                return false;
+                
+        }
+          
+        bool operator>=(const dataType & realdata){
+            bool res=false;
+            if(this->type == realdata.type){
+                if(this->type == Int) {
+                    if(this->data.dInt >= realdata.data.dInt) res=true;
+                    else res=false;
+                }
+                else if(this->type == Float) {
+                    if(this->data.dFloat >= realdata.data.dFloat) res=true;
+                    else res=false;
+                }
+                else if(this->type == Bool) {
+                    if(this->data.dBool >= realdata.data.dBool) res=true;
+                    else res=false;
+                }
+               return res;
+            }
+            else 
+                return false;
+        }
+    };  //...
+      
+    int IP=0;
+    int countINS=0;
+    dataType R[15];
+    dataType static_Mem[vnMemory];
+    vector<vector<string>> readINS;
+    vector<int> Stack;
+    void loadFile(string filename);
+    dataType srcData(string src);
+    void execuProgram();
+    void run(string filename);
 };
- 
+
+
+
 
 #endif
